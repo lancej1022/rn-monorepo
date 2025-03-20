2c3ee25a08ab5640ba37f5a1a96b99fd3c240e25
lancej1022 <46359098+lancej1022@users.noreply.github.com>
2025-03-19 22:19:36 -0700

    Working state

--- apps/vite-rnr-app/src/main.tsx
+++ apps/vite-rnr-app/src/main.tsx
@@ -4,33 +4,33 @@ import { createRoot } from 'react-dom/client';
 // import './index.css'
 import App from './App';
 // Import the generated route tree
-import { routeTree } from './routeTree.gen';
+// import { routeTree } from './routeTree.gen';
 import { ThemeProvider } from './ThemeProvider';
 import { createRouter } from '@tanstack/react-router';
 
 // Create a new router instance
-const router = createRouter({
-  routeTree,
-  context: {},
-  defaultPreload: 'intent',
-  scrollRestoration: true,
-  defaultStructuralSharing: true,
-  defaultPreloadStaleTime: 0,
-});
+// const router = createRouter({
+//   routeTree,
+//   context: {},
+//   defaultPreload: 'intent',
+//   scrollRestoration: true,
+//   defaultStructuralSharing: true,
+//   defaultPreloadStaleTime: 0,
+// });
 
 // Register the router instance for type safety
-declare module '@tanstack/react-router' {
-  interface Register {
-    router: typeof router;
-  }
-}
+// declare module '@tanstack/react-router' {
+//   interface Register {
+//     router: typeof router;
+//   }
+// }
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     {/* <ThemeProvider>
       <App />
     </ThemeProvider> */}
-    <RouterProvider router={router} />
+    {/* <RouterProvider router={router} /> */}
     <App />
   </StrictMode>
 );
