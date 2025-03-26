1bd195ad86bb475c08d22223797ee640b5449f3d
lancej1022 <46359098+lancej1022@users.noreply.github.com>
2025-03-25 21:57:59 -0700

    Move `LoginForm` out of `chat-app` and into the `features` package.

--- apps/chat-app/src/routes/signup.tsx
+++ apps/chat-app/src/routes/signup.tsx
@@ -1,15 +1,12 @@
 import { createFileRoute } from '@tanstack/react-router';
-// import { Login, LoginForm } from 'features';
+import { LoginForm } from 'features';
 import { GalleryVerticalEnd } from 'lucide-react-native';
-import { LoginForm } from '~/components/LoginForm';
 
 export const Route = createFileRoute('/signup')({
   component: RouteComponent,
 });
 
 function RouteComponent() {
-  // return <Login />;
-
   return (
     <div className='grid min-h-svh lg:grid-cols-2'>
       <div className='flex flex-col gap-4 p-6 md:p-10'>
